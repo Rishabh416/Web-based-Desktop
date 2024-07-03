563bc6dee13b91ffae4c6c1a60516719483930a5
rishabh416 <mittalr2008@gmail.com>
2024-07-03 14:16:40 +0400

    Added drag functionality

--- WebOS/src/apps/welcome.jsx
+++ WebOS/src/apps/welcome.jsx
@@ -1,9 +1,14 @@
+import Draggable, {DraggableCore} from 'react-draggable';
+
 function welcome() {
+
     return(
-        <div className="welcome">
-            <h1>Hello</h1>
-            <p>Welcome to my website. Here you will be able to explore all about my work, projects and interests whilst having a great time interacting in this web-based desktop environment</p>
-        </div>
+        <Draggable>
+            <div className="app" id="welcome">
+                <h1>Hello</h1>
+                <p>Welcome to my website. Here you will be able to explore all about my work, projects and interests whilst having a great time interacting in this web-based desktop environment</p>
+            </div>
+        </Draggable>
     )
 }
 
