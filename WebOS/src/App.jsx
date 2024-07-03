db3ee6f1e7e61826258e82e61da2301cc2e06903
rishabh416 <mittalr2008@gmail.com>
2024-07-03 14:03:56 +0400

    added welcome app

---
+++ WebOS/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Welcome from './apps/welcome.jsx'
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
---
+++ WebOS/src/apps/welcome.jsx
@@ -0,0 +1,10 @@
+function welcome() {
+    return(
+        <div className="welcome">
+            <h1>Hello</h1>
+            <p>Welcome to my website. Here you will be able to explore all about my work, projects and interests whilst having a great time interacting in this web-based desktop environment</p>
+        </div>
+    )
+}
+
+export default welcome
